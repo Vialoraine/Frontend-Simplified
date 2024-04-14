0d5d1ea5f1cd630f2ea19f69cfe112f60b69f7da
Loreina Salalila <vialoraine.gg@gmail.com>
2024-04-14 06:52:02 -0700

    JavaScript Crash Course: String Notes

---
+++ Week 3 JavaScript/1 Theory/2 Crash Course/notes.js
@@ -0,0 +1,7 @@
+// Exercise find the correct index for the letter 'd' below
+
+console.log('Hello World'[10]) 
+// => will console log 'd'
+
+console.log('12345'.length)
+// => will console log the length of the data string
