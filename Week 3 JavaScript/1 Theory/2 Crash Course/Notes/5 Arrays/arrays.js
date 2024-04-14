61716472841a08f1d752ef775cbcb015a55328ea
Loreina Salalila <vialoraine.gg@gmail.com>
2024-04-14 11:34:45 -0700

    JavaScript Crash Course: (Arrays Example) Notes

---
+++ Week 3 JavaScript/1 Theory/2 Crash Course/Notes/5 Arrays/arrays.js
@@ -0,0 +1,31 @@
+// let item1 = 20
+// let item2 = 30
+// let item3 = 40
+// let item4 = 50
+// let item5 = 100
+
+// let arr = [] => initializes an empty array
+
+// let arr = [20, 30, 40, 50, 100];
+
+// first element of array
+// console.log(arr[0])
+
+// last element of array
+// console.log(arr[4]) => console.log(arr[arr.length -1])
+
+// console.log(arr[arr.length -1])
+
+// adding elements onto end of array
+// arr.push(200)
+
+// 
+// let newArr = arr.filter(element => {
+//   console.log(element);
+//   if (element < 50) {
+//     return true;
+//   }
+// });
+
+// console.log(newArr);
+
