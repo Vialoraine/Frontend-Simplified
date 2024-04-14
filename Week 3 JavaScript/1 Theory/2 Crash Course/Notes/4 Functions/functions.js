d81904995b64d635a435747f9444072d4b9d06a2
Loreina Salalila <vialoraine.gg@gmail.com>
2024-04-14 11:01:18 -0700

    JavaScript Crash Course: (Functions Example) Celcius to Farenheit Exercise

---
+++ Week 3 JavaScript/1 Theory/2 Crash Course/Notes/4 Functions/functions.js
@@ -0,0 +1,21 @@
+// DRY - Don't repeat yourself
+
+
+// Function definition
+// function welcomePersonToFES(firstName, lastName) {
+  // console.log("Welcome to FES, David");
+//   console.log(`Welcome to FES, ${firstName} ${lastName}`)
+// }
+
+// Call the function
+// welcomePersonToFES('David','Bragg');
+// welcomePersonToFES('Zen');
+// welcomePersonToFES('Mitri');
+
+// function sumOfTwoNumbers(num1, num2) {
+//   return num1 + num2
+// }
+
+// console.log(sumOfTwoNumbers(10,10));
+
+// Function Exercise: 
