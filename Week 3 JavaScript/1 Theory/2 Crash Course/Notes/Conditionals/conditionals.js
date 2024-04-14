73eb216bf7ac44bb0aa81730def535b8ec6354be
Loreina Salalila <vialoraine.gg@gmail.com>
2024-04-14 07:58:58 -0700

    JavaScript Crash Course: (Conditional Example) showing bootcamp content

---
+++ Week 3 JavaScript/1 Theory/2 Crash Course/Notes/Conditionals/conditionals.js
@@ -0,0 +1,19 @@
+// FES Example
+
+// if logged in show the video
+// let subscribed = false
+// let loggedIn = true
+
+// if (subscribed === true) {
+//   console.log('show the video')
+// }
+// if user is logged in and subscribed show the video
+// else if (loggedIn === true) {
+//   console.log('tell the user to upgrade their subscription')
+// }
+// if the user is logged then tell them to upgrade their subscription
+// else {
+//   console.log('tell user to log into account')
+// }
+// if the user is not logged in then tell them to log into their account
+
