05e3b26050d6d71719c23ed815057e2fbacb6f2b
Loreina Salalila <vialoraine.gg@gmail.com>
2024-04-15 10:28:00 -0700

    (Day 2) JavaScript Crash Course: reviewed data types, variables, and conditionals

---
+++ Week 3 JavaScript/1 Theory/2 Crash Course/Day 2/exercises.js
@@ -0,0 +1,42 @@
+// Q: console.log('Hello World') and log the last character of the string
+// A: console.log('Hello World'[10]);
+
+/*  Q: Converting Temperatures
+    Celsius to Fahrenheit formula => F = C x 1.8 +32
+
+      let celcius = 10;
+      let fahrenheit = => convert celcius to fahrenheit
+
+      console.log() => print out the result
+    A:  
+    let celcius = 10
+    let fahrenheit = celcius * 1.8 + 32
+    console.log(fahrenheit)
+*/ 
+
+/*  Q: Using JavaScript in the string print, print the change total values for the
+       following statements:
+    A:
+    let cash = 60
+    let price = 40
+    let difference = cash - price
+
+    if (cash > price) {
+      console.log(`you paid extra - here's ${difference} dollars change`);
+    } else if (cash === price) 
+    {
+      console.log("you paid the exact amount, have a nice day!");
+    } else {
+      console.log(`not enough money - you still owe ${difference} dollars`);
+    }
+*/
+
+/*  Q: 'give reciept' : 'do not give reciept'
+    A:
+    let cash = 50;
+    let price = 40;
+    let isStoreOpen = true;
+
+    let str = cash>= price && isStoreOpen ? 'give reciept' : 'do not give reciept'
+    console.log(str)
+*/
