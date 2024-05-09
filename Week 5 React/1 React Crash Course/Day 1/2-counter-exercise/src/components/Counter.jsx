653c757995e2b18a6f525242f6d701cbc9242d6a
Loreina Salalila <vialoraine.gg@gmail.com>
2024-05-09 03:18:17 -0700

    Reviewed Intro to React, Components, Props, Event Handlers, and Hooks

--- Week 5 React/1 React Crash Course/Day 1/2-counter-exercise/src/components/Counter.jsx
+++ Week 5 React/1 React Crash Course/Day 1/2-counter-exercise/src/components/Counter.jsx
@@ -1,27 +1,107 @@
-import { React, useState } from "react";
+// Create a "Counter.jsx" component
+// Create a default `count` of 0
+// Create a button to increment `count` by 1
+// Create a button to decrement `count` by 1
+// Import your Counter in App.jsx and test it
 
-function Counter() {
-  const [counter, setCounter] = useState(0);
+// Using Numbers in React: 
 
-  const increment = () => {
-    setCounter((count) => count + 1);
-  };
+  // import { React, useState } from "react";
 
-  const decrement = () => {
-    setCounter((count) => count - 1);
-  };
+  // function Counter() {
+  //   const [counter, setCounter] = useState(0);
 
-  return (
-    <div className="counter-wrapper">
-      <button className="decrement-btn" onClick={decrement}>
-        -
-      </button>
-      <span className="counter__output">{counter}</span>
-      <button className="increment-btn" onClick={increment}>
-        +
-      </button>
-    </div>
-  );
-}
+  //   const increment = () => {
+  //     setCounter((count) => count + 1);
+  //   };
 
-export default Counter;
+  //   const decrement = () => {
+  //     setCounter((count) => count - 1);
+  //   };
+
+  //   return (
+  //     <div className="counter-wrapper">
+  //       <button className="decrement-btn" onClick={decrement}>
+  //         -
+  //       </button>
+  //       <span className="counter__output">{counter}</span>
+  //       <button className="increment-btn" onClick={increment}>
+  //         +
+  //       </button>
+  //     </div>
+  //   );
+  // }
+
+  // export default Counter;
+
+
+
+// Using Arrays in React: with a Reset Button:
+
+  // import { React, useState } from "react";
+
+  // function Counter() {
+  //   const [arr, setArr] = useState([]);
+
+  //   function addPlus() {
+  //     setArr((prevArr) => [...prevArr, "+"]);
+  //   }
+
+  //   function addMinus() {
+  //     setArr((prevArr) => [...prevArr, "-"]);
+  //   }
+
+  //   function reset() {
+  //     setArr([]);
+  //   }
+
+  //   return (
+  //     <div className="counter-wrapper">
+  //       <button onClick={addMinus}> - </button>
+  //       <button onClick={addPlus}> + </button>
+  //       <button onClick={reset}> Reset </button>
+  //       {arr.toString()}
+  //     </div>
+  //   );
+  // }
+
+  // export default Counter;
+
+
+
+// Using Objects in React:
+
+  // 1. Use a callback to get the previous value
+  // 2. Spread out all the properties of the previous state
+  // 3. Only change the property that you need to change
+
+  import { React, useState } from "react";
+
+  function Counter() {
+    const [cart, setCart] = useState({ item: "Apple", quantity: 0 });
+
+    function addApple() {
+      setCart((prevCart) => ({
+        ...prevCart,
+        quantity: prevCart.quantity + 1,
+      }));
+    }
+
+    function remApple() {
+      setCart((prevCart) => ({
+        ...prevCart,
+        quantity: prevCart.quantity - 1,
+      }));
+    }
+
+    return (
+      <div className="counter-wrapper">
+        <button onClick={remApple}>-</button>
+        {cart.quantity}
+        {cart.item}
+        <button onClick={addApple}>+</button>
+      </div>
+    );
+  }
+
+  export default Counter;
