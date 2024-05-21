45a3005144bd8250aee042f11336b76e0fb4933e
Loreina Salalila <vialoraine.gg@gmail.com>
2024-05-21 09:51:39 -0700

    Firestore: How to Create

--- Week 5 React/5 Firestore/firebase-firestore-practice/src/firebase/init.js
+++ Week 5 React/5 Firestore/firebase-firestore-practice/src/firebase/init.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import { getAuth } from 'firebase/auth'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -11,9 +12,10 @@ const firebaseConfig = {
   projectId: "fir-practice-b2c02",
   storageBucket: "fir-practice-b2c02.appspot.com",
   messagingSenderId: "688145282502",
-  appId: "1:688145282502:web:4d9edf18bab310c58faec7",
+  appId: "1:688145282502:web:25d17ceb6c12ec9c8faec7",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
+export const db = getFirestore();
